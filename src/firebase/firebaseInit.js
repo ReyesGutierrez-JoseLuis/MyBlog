41a03134a56174f434eed06c7e2ce906b62e9167
José Luis Reyes Gutiérrez <56399319+ReyesGutierrez-JoseLuis@users.noreply.github.com>
2021-10-19 19:57:06 -0500

    Update firebaseInit.js

--- src/firebase/firebaseInit.js
+++ src/firebase/firebaseInit.js
@@ -2,12 +2,12 @@ import firebase from "firebase/app";
 import "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBZDDJeuLcv0wkjPU2FhHt2tI4z4lYmVHM",
-  authDomain: "blogyt-c9c94.firebaseapp.com",
-  projectId: "blogyt-c9c94",
-  storageBucket: "blogyt-c9c94.appspot.com",
-  messagingSenderId: "1055993676551",
-  appId: "1:1055993676551:web:fcd6440f47677c83d57ba2",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
